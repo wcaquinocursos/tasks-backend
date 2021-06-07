f08cf454012a8f816e01727ca71d1fa73bc09203
jcsaGitHub <49504447+jcsaGitHub@users.noreply.github.com>
2021-06-07 12:47:38 -0300

    Add files via upload

---
+++ TaskControllerTest.java
@@ -0,0 +1,80 @@
+package br.ce.wcaquino.taskbackend.utils;
+
+import java.time.LocalDate;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import br.ce.wcaquino.taskbackend.controller.TaskController;
+import br.ce.wcaquino.taskbackend.model.Task;
+import br.ce.wcaquino.taskbackend.repo.TaskRepo;
+import junit.framework.Assert;
+
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.MockitoAnnotations;
+
+public class TaskControllerTest {
+	
+	@Mock
+	private TaskRepo taskRepo;
+	
+	@InjectMocks
+	private TaskController controller;
+	
+	@Before
+	public void setup() {
+		MockitoAnnotations.initMocks(this);
+	}
+	
+	@Test
+	public void naoDeveSalvarTarefaSemDescricao() {
+		Task todo = new Task();
+		todo.setDueDate(LocalDate.now());
+		try {
+			controller.save(todo);
+			Assert.fail("Não deveria chegar nesse ponto!");
+		} catch (ValidationException e) {
+			Assert.assertEquals("Fill the task description", e.getMessage());
+		}
+	}
+	
+	@Test
+	public void naoDeveSalvarTarefaSemData() {
+		Task todo = new Task();
+		todo.setTask("Descrição");
+		//todo.setDueDate(LocalDate.now());
+		try {
+			controller.save(todo);
+			Assert.fail("Não deveria chegar nesse ponto!");
+		} catch (ValidationException e) {
+			Assert.assertEquals("Fill the due date", e.getMessage());
+		}
+
+	}
+	
+	@Test
+	public void naoDeveSalvarTarefaComDataPassada() {
+		Task todo = new Task();
+		todo.setTask("Descricao");
+		todo.setDueDate(LocalDate.of(2010, 01, 01));
+		try {
+			controller.save(todo);
+			Assert.fail("Não deveria chegar nesse ponto!");
+		} catch (ValidationException e) {
+			Assert.assertEquals("Due date must not be in past", e.getMessage());
+		}		
+	}
+	
+	@Test
+	public void deveSalvarTarefaComSucesso() throws ValidationException {
+		Task todo = new Task();
+		todo.setTask("Descricao");
+		todo.setDueDate(LocalDate.now());
+		controller.save(todo);
+		
+		Mockito.verify(taskRepo).save(todo);
+	}
+
+}
