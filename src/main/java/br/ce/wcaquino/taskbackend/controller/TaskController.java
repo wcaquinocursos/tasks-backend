8b6279c4ff59d0ae837e5c18fde36a5d052b52a6
CarlosGeorge <carlosgeorge248@gmail.com>
2021-10-02 16:49:07 -0300

    Feature: Remove tasks

--- src/main/java/br/ce/wcaquino/taskbackend/controller/TaskController.java
+++ src/main/java/br/ce/wcaquino/taskbackend/controller/TaskController.java
@@ -5,7 +5,9 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -42,4 +44,13 @@ public class TaskController {
 		Task saved = todoRepo.save(todo);
 		return new ResponseEntity<Task>(saved, HttpStatus.CREATED);
 	}
+	
+	@DeleteMapping("/{id}")
+	public ResponseEntity<Task> delete(@PathVariable Long id) {
+		
+		todoRepo.deleteById(id);
+		
+		return ResponseEntity.noContent().build();
+	}
+	
 }
