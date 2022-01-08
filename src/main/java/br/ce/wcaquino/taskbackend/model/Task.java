6c2e25f4e1d62f5d8ae2580f2969c87e9f5a8199
Luis Matos <luis.felipe.pereira.matos@gmail.com>
2022-01-07 22:25:37 -0300

    ajustes

--- src/main/java/br/ce/wcaquino/taskbackend/model/Task.java
+++ src/main/java/br/ce/wcaquino/taskbackend/model/Task.java
@@ -5,6 +5,7 @@ import java.time.LocalDate;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
 @Entity
@@ -21,7 +22,7 @@ public class Task {
 	public Task() {}
 
 	@Id
-	@GeneratedValue
+	 @GeneratedValue(strategy = GenerationType.IDENTITY)
 	public Long getId() {
 		return id;
 	}
