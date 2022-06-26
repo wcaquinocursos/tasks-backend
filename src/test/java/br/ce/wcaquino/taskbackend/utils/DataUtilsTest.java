8bc5ef465a6d478544d6950c0fab364f201c9dc8
Orlando <orlando.junior07@hotmail.com>
2022-06-26 17:32:02 -0300

    Testes unitários da aplicação

---
+++ src/test/java/br/ce/wcaquino/taskbackend/utils/DataUtilsTest.java
@@ -0,0 +1,30 @@
+package br.ce.wcaquino.taskbackend.utils;
+
+import java.time.LocalDate;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class DataUtilsTest {
+	
+	@Test
+	public void deveRetoornarTrueParaDatasFuturas() {
+		LocalDate date = LocalDate.of(2030, 01, 01);
+		
+		Assert.assertTrue(DateUtils.isEqualOrFutureDate(date));
+	}
+	
+	@Test
+	public void deveRetoornarFalseParaDatasPassadas() {
+		LocalDate date = LocalDate.of(2010, 01, 01);
+		
+		Assert.assertFalse(DateUtils.isEqualOrFutureDate(date));
+	}
+	
+	@Test
+	public void deveRetoornarTrueParaDataAtual() {
+		LocalDate date = LocalDate.now();
+		
+		Assert.assertTrue(DateUtils.isEqualOrFutureDate(date));
+	}
+}
