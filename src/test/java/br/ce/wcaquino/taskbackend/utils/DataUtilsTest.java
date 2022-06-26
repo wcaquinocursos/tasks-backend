855e451b2749312a24239ca95937ca47e9000995
Orlando <orlando.junior07@hotmail.com>
2022-06-26 18:42:25 -0300

    Testando os testes

--- src/test/java/br/ce/wcaquino/taskbackend/utils/DataUtilsTest.java
+++ src/test/java/br/ce/wcaquino/taskbackend/utils/DataUtilsTest.java
@@ -11,7 +11,7 @@ public class DataUtilsTest {
 	public void deveRetoornarTrueParaDatasFuturas() {
 		LocalDate date = LocalDate.of(2030, 01, 01);
 		
-		Assert.assertTrue(DateUtils.isEqualOrFutureDate(date));
+		Assert.assertFalse(DateUtils.isEqualOrFutureDate(date));
 	}
 	
 	@Test
