67535e0e99c1c21192ab0c44e0bd771442684940
Marcel Barbosa <marcel.sby.br@gmail.com>
2022-11-03 17:36:34 -0300

    tests: DateUtils

---
+++ src/test/java/br/ce/wcaquino/taskbackend/utils/DateUtilsTest.java
@@ -0,0 +1,27 @@
+package br.ce.wcaquino.taskbackend.utils;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.time.LocalDate;
+import org.junit.Test;
+
+public class DateUtilsTest {
+
+  @Test
+  public void deveRetornarTrueParaDatasFuturas() {
+    LocalDate date = LocalDate.of(2099,1, 1);
+    assertThat(DateUtils.isEqualOrFutureDate(date)).isTrue();
+  }
+
+  @Test
+  public void deveRetornarFalseParaDatasPassadas() {
+    LocalDate date = LocalDate.of(2001,1, 1);
+    assertThat(DateUtils.isEqualOrFutureDate(date)).isFalse();
+  }
+
+  @Test
+  public void deveRetornarTrueParaDataAtual() {
+    LocalDate date = LocalDate.now();
+    assertThat(DateUtils.isEqualOrFutureDate(date)).isTrue();
+  }
+}
