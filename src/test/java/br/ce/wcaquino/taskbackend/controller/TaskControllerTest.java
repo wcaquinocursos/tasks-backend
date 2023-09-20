a79c3bcc992ebf2517e2d55f22f347df290c3ecb
Caio Cesar <caiocesar.moraes@hotmail.com>
2023-09-20 11:23:51 -0300

    corrigindo import

--- src/test/java/br/ce/wcaquino/taskbackend/controller/TaskControllerTest.java
+++ src/test/java/br/ce/wcaquino/taskbackend/controller/TaskControllerTest.java
@@ -15,36 +15,36 @@ import br.ce.wcaquino.taskbackend.repo.TaskRepo;
 import br.ce.wcaquino.taskbackend.utils.ValidationException;
 
 public class TaskControllerTest {
-	
+
 	@Mock
 	private TaskRepo taskRepo;
-	
+
 	@InjectMocks
 	private TaskController controller;
-	
+
 	@Before
 	public void setup() {
 		MockitoAnnotations.initMocks(this);
 	}
-	
+
 	@Test
-	public void nãoDeveSalvarTarefasSemDescricao()  {
+	public void nãoDeveSalvarTarefasSemDescricao() {
 		Task todo = new Task();
 		todo.setDueDate(LocalDate.now());
-				try {
+		try {
 			controller.save(todo);
 			Assert.fail("Não deveria chegar nesse ponto");
 		} catch (ValidationException e) {
 			Assert.assertEquals("Fill the task description", e.getMessage());
 		}
 	}
-	
+
 	@Test
 	public void nãoDeveSalvarTarefasSemData() {
 		Task todo = new Task();
 		todo.setTask("Descricao");
-		//todo.setDueDate(LocalDate.now());
-		
+		// todo.setDueDate(LocalDate.now());
+
 		try {
 			controller.save(todo);
 			Assert.fail("Não deveria chegar nesse ponto");
@@ -53,24 +53,23 @@ public class TaskControllerTest {
 		}
 	}
 
-
 	@Test
 	public void nãoDeveSalvarTarefasComDataPassada() {
-		
+
 		Task todo = new Task();
 		todo.setTask("Descricao");
 		todo.setDueDate(LocalDate.of(2010, 01, 01));
-		//Caio
-		
+		// Caio
+
 		try {
 			controller.save(todo);
 			Assert.fail("Não deveria chegar nesse ponto");
 		} catch (ValidationException e) {
 			Assert.assertEquals("Due date must not be in past", e.getMessage());
 		}
-		
+
 	}
-	
+
 	@Test
 	public void DeveSalvarTarefasComSucesso() throws ValidationException {
 		Task todo = new Task();
@@ -78,9 +77,7 @@ public class TaskControllerTest {
 		todo.setDueDate(LocalDate.now());
 		controller.save(todo);
 		Mockito.verify(taskRepo).save(todo);
-	
-	}
-		
-	
-}
 
+	}
+
+}
--- src/test/java/br/ce/wcaquino/taskbackend/utils/DateUtilsTest.java
+++ src/test/java/br/ce/wcaquino/taskbackend/utils/DateUtilsTest.java
@@ -6,26 +6,25 @@ import org.junit.Assert;
 import org.junit.Test;
 
 public class DateUtilsTest {
-	
+
 	@Test
 	public void deveRetornarTrueParaDatasFuturas() {
 		LocalDate date = LocalDate.of(2030, 01, 01);
 		Assert.assertTrue(DateUtils.isEqualOrFutureDate(date));
-		
+
 	}
-	
 
 	@Test
 	public void deveRetornarTrueParaDatasPassadas() {
 		LocalDate date = LocalDate.of(2010, 01, 01);
 		Assert.assertFalse(DateUtils.isEqualOrFutureDate(date));
-		
+
 	}
-	
+
 	@Test
 	public void deveRetornarTrueParaDatasAtual() {
 		LocalDate date = LocalDate.now();
 		Assert.assertTrue(DateUtils.isEqualOrFutureDate(date));
-		
+
 	}
 }
